9a6c257ca41554c244990d1f91624722ec8a6de6
chiluksr <chiluksr@oregonstate.edu>
2022-12-02 23:03:25 -0800

    updated swagger

--- lambda_function.py
+++ lambda_function.py
@@ -1,21 +1,42 @@
-import boto3
 import json
-from boto3.dynamodb.conditions import Key,Attr
+import boto3
+
 def lambda_handler(event, context):
-        shahHash = event['params']['path']['shaHash']
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table('decrypt')      
-        resp = table.query(KeyConditionExpression=Key('key').eq(shahHash))
-        print(resp)
-        count = resp['Count'] 
-        print("Count",count)
-        if count == 0: 
-          statusCode=404
-          password=""
-          return {'statusCode': statusCode}
-        else:
-          password = resp['Items'][0]
-          hash =password['key']
-          password = password['value']
-          statusCode=200
-          return {'statusCode': statusCode,'body': {hash : password}}
+    responseflag = 0
+    hash_key=event['path'][1:]
+    s3_connect=boto3.client("s3")
+    response=s3_connect.select_object_content(
+        Bucket='decrypterservice',
+        Key='output.csv',
+        ExpressionType='SQL',
+        Expression=f"Select s.password From s3object s where s.shaHash='{hash_key}'",
+        InputSerialization={'CSV': {"FileHeaderInfo": "Use"}, 'CompressionType': 'NONE'},
+        OutputSerialization={'CSV': {}},
+    )
+    print(response)
+    for event in response['Payload']:
+        if 'Records' in event:
+            records = event['Records']['Payload'].decode('utf-8')
+            responseflag=1
+            print(records)
+        elif 'Stats' in event:
+            statsDetails = event['Stats']['Details']
+            print("Stats details bytesScanned: ")
+            print(statsDetails['BytesScanned'])
+            print("Stats details bytesProcessed: ")
+            print(statsDetails['BytesProcessed'])
+            print("Stats details bytesReturned: ")
+            print(statsDetails['BytesReturned'])
+    
+    if responseflag == 1 :
+        s=records.rstrip()
+        return {
+            'statusCode': 200,
+            'body': json.dumps({hash_key:s})
+        }
+    else :
+        return {
+            'statusCode': 404,
+            'body': ""
+        }
+        
